167a65a5629a9adf8d8511503195b0403cb661aa
Elijah (Eli) Ruffin <ruffineli77@gmail.com>
2024-02-29 14:29:15 -0500

    Delete frontend directory
    I have a nearly complete version of this directory to replace these files.

--- frontend/src/utils/AboutArray.jsx
+++
@@ -1,9 +0,0 @@
-
-const AboutArray = [
-    'I am a self-taught full-stack developer proficient in Django, Flask, JavaScript, and React, and I have a strong foundation in database management with PostgreSQL and SQLite.',
-    'I enjoy creating applications that employ natural language processing and AI to engage the users of my apps in unique and creative ways (see my Mythology Project).',
-    'I am originally from New York and I grew up in Baltimore Maryland. I plan to take my skills to California, New York, or Florida.',
-    'In my free time I love playing baseball, swimming (In the ocean), hiking, working out, studying mythology and baking delicious desserts.',
-];
-
-export default AboutArray;
--- frontend/src/utils/SkillsArray.jsx
+++
@@ -1,8 +0,0 @@
-
-const SkillsArray = [
-    'Python', 'JavaScript', 'HTML','CSS', 'Django', 'Flask', 'ExpressJS', 'NodeJS', 'NextJS', 'SASS', 'React',
-    'Angular', 'Vue', 'Material UI', 'Tailwind', 'Bootstrap', 'JSON', 'REST', 'SOAP', 'PostgreSQL', 'SQLite',
-    'MySQL', 'MongoDB', 'Linux', 'Windows', 'Mac'
-];
-
-export default SkillsArray;
